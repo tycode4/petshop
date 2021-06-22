1210a367b93b015fd800c45929e4d71abd8a31b9
ty <kim@tyui-MacBookAir.local>
2021-06-22 17:40:46 +0900

    ADD: Movie Application

--- petshop/urls.py
+++ petshop/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path, include
 
-
 urlpatterns = [
-	path('owners', include('owners.urls'))
+	path('owners', include('owners.urls')),
+	path('movie', include('movie.urls'))
 ]
 
 
